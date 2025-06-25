f3661096ce77d3ec5159157845f4c167007ff5e0
Abi Ali Qutbuddin <abialigadi@gmail.com>
2025-06-26 01:20:31 +0530

    .

--- server.js
+++ server.js
@@ -3050,7 +3050,7 @@ app.get('/api/aiut/:tableName', async (req, res) => {
 
 async function populateStudentGoods(financial_survey_id, student_id, assetsOptions, created_by_id) {
   console.log('⚙️  populateStudentGoods start', {
-    inancial_survey_id,
+    financial_survey_id,
     student_id,
     assetsOptions,
     created_by_id
